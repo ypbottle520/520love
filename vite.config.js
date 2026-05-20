af27aa1051ddebf7649caf996f3beaeaaaad3942
ypbottle520 <ypbottle520@gmail.com>
2026-05-20 10:41:31 +0800

    配置 GitHub Pages 上线

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
-import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-})
+  base: "/520love/",
+});
